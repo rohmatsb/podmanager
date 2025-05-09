7e0f18cbdd73e4ae5a04fd361f16b04d7780e740
Rohmat Subekti <rohmatsb@gmail.com>
2025-05-09 14:25:53 +0000

    menambahkan opsi menu menggunakan case

--- menu/podmanager.sh
+++ menu/podmanager.sh
@@ -4,18 +4,38 @@ clear
 # Store value 
 script_version="1.0"
 
-# Print menu
-echo "==============="
-echo "PODMAN MANAGER"
-echo "==============="
-echo "STATUS"
-echo "------"
-echo "script-version   : ${script_version}"
-echo "podman-version   : $(podman --version | awk '{print $3}')"
-echo "==============="
-echo "1. Create container"
-echo "2. List all existing container"
-echo "3. Delete container"
-echo "==============="
-echo ""
-echo "CTRL + C to exit"
+# Menggunakan while-true, agar terjadi pengulangan jika user input invalid
+while true; do
+
+    # Print menu
+    echo "==============="
+    echo "PODMAN MANAGER"
+    echo "==============="
+    echo "STATUS"
+    echo "------"
+    echo "script-version   : ${script_version}"
+    echo "podman-version   : $(podman --version | awk '{print $3}')"
+    echo "==============="
+    echo "1. Create container"
+    echo "2. List all existing container"
+    echo "3. Delete container"
+    echo "4. Exit"
+    echo "==============="
+    echo ""
+
+    # Menerima input pengguna
+    read -p "Enter number : " pilihan
+
+    # Sortir input
+    case $pilihan in
+    1) clear && create-container ; break ;;
+    2) clear && list-container ; break ;;
+    3) clear && delete-container ; break ;;
+    4) exit ;;
+    *) clear && echo "Silakan masukkan pilihan yang benar!" && sleep 3 && clear ;;
+
+    # Menutup case
+    esac
+
+# Menutup while-true
+done
