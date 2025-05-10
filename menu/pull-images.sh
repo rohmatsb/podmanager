406cca32dfaed72ea1cbeff7432121c5b7a6bbbc
Rohmat Subekti <rohmatsb@gmail.com>
2025-05-10 16:58:58 +0000

    buat menu pull image (unfinished)

---
+++ menu/pull-images.sh
@@ -0,0 +1,19 @@
+# Clear whatever in the screen
+clear
+
+# Simpan url pull ke dalam variabel
+debian="quay.io/toolbx-images/debian-toolbox"
+arch="quay.io/toolbx/arch-toolbox"
+centos="quay.io/toolbx-images/centos-toolbox"
+alpine="quay.io/toolbx-images/alpine-toolbox"
+rocky="quay.io/toolbx-images/rockylinux-toolbox"
+
+# Print menu
+echo "==============="
+echo "PULL IMAGES"
+echo "==============="
+echo "1. Debian latest"
+echo "2. Arch latest"
+echo "3. Centos latest"
+echo "4. Alpine latest"
+echo "5. RockyLinux latest"
