7717a6d32507d86c1508745698a7f4170dbe6565
Rohmat Subekti <rohmatsb@gmail.com>
2025-06-16 03:41:59 +0000

    menyelesaikan menu pull image

--- menu/podmanager-pull-images.sh
+++ menu/podmanager-pull-images.sh
@@ -15,15 +15,37 @@ while true; do
     echo "==============="
     echo "PULL IMAGES"
     echo "==============="
+    echo "0. Custom URL"
     echo "1. Debian latest"
     echo "2. Arch latest"
     echo "3. Centos latest"
     echo "4. Alpine latest"
     echo "5. RockyLinux latest"
+ 
 
     # Menerima input user
     read -p "Enter number : " pilihan
 
     # Sortir input
     case $pilihan in
-    1) 
+    0) 
+        clear
+        echo "========"
+        echo "Enter url to pull container images!"
+        echo "Example : 'quay.io/toolbx-images/debian-toolbox'"
+        echo "========"
+        echo ""
+        read -p "Enter url : " custom_url
+        podman pull $custom_url
+        break # Menghentikan loop, karena pilihan sudah benar
+        ;;
+    1) clear && podman pull $debian ; break ;;
+    2) clear && podman pull $arch ; break ;;
+    3) clear && podman pull $centos ; break ;;
+    4) clear && podman pull $alpine ; break ;;
+    5) clear && podman pull $rocky ; break ;;
+    *) clear && echo "Silakan masukkan pilihan yang benar!" && sleep 3 && clear ;;
+    esac # Menutup case
+
+# Menutup while-true
+done
