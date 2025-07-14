f8e14b7b1fc42912d51c1de5f502bb4fb1a275b6
Rohmat Subekti <rohmatsb@gmail.com>
2025-07-14 14:35:01 +0000

    buat file pembuatan container podman

---
+++ menu/podmanager-create-container.sh
@@ -0,0 +1,14 @@
+# clear dulu
+clear
+
+# Menggunakan while-true agar terjadi pengulangan jika nilai yang dimasukkan pengguna salah
+while true; do
+
+# Menampilkan menu
+echo "========"
+echo "Create container
+echo "========"
+echo ""
+read -p "Enter container name : " nama_container
+read -p "Enter container port to forward : " container_port
+read -p "Enter host port : " host_port
