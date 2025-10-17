dc8216136e2f4af744d63f2391d76a15c2677586
Rohmat Subekti <rohmatsb@gmail.com>
2025-10-17 03:37:34 +0000

    add enter terminal container menu

---
+++ menu/podmanager-enter-container.sh
@@ -0,0 +1,86 @@
+# Color
+NC='\e[0m' # No Color (reset)
+RED='\e[0;31m'
+GREEN='\e[0;32m'
+YELLOW='\e[0;33m'
+BLUE='\e[0;34m'
+MAGENTA='\e[0;35m'
+CYAN='\e[0;36m'
+WHITE='\e[0;37m'
+
+# Mendeklarasikan variabel global
+akan_dibuka=""
+
+# Mendefinisikan fungsi-fungsi global
+function menu_masuk {
+    # memasukkan daftar nama container kedalam array
+    while true; do
+        clear
+        echo "================="
+        echo "Enter containers"
+        echo "================="
+        echo ""
+        echo "Pilih container yang akan dibuka terminal nya :"
+        echo ""
+
+        # memasukkan daftar container yang ada ke dalam array
+        mapfile -t daftar_podman_container < <(podman ps -a --format "{{.Names}}")
+
+        # print pesan yang akan ditampilkan di bawah menu (saat input)
+        PS3="Masukkan angka pilihan : "
+        
+        # print pilihan menggunakan select
+        select cont in "${daftar_podman_container[@]}"; do
+            if [[ -n "${cont}" ]]; then
+                akan_dihapus=${cont}
+                break 2
+            else
+                echo ""
+                echo -e "${RED}Input invalid, silahkan masukkan ulang!${NC}"
+                sleep 3
+                continue 2
+            fi
+        done
+    done
+}
+
+function yn_enter_container {
+    clear
+    echo "================="
+    echo "Enter container"
+    echo "================="
+    echo ""
+    echo -e "Yakin ingin membuka container ${akan_dibuka}?"
+    echo ""
+    echo "Enter y/n/yes/no (case insensitive)"
+    read -p "Masukkan pilihan : " yn_enter
+}
+
+function entering {
+    podman #### ${akan_dibuka} #aksi membuka
+
+    clear
+    echo "================="
+    echo "Enter container"
+    echo "================="
+    echo ""
+    echo -e "Membuka container ${akan_dibuka}"
+}
+
+###################################################
+### Selesai mendefinisikan fungsi-fungsi global ###
+###################################################
+
+menu_masuk
+yn_enter_container
+
+if [[ "${yn_enter,,}" == "yes" || "${yn_enter,,}" == "y" ]]; then
+    entering
+elif [[ "${yn_enter,,}" == "no" || "${yn_enter,,}" == "n" ]]; then
+    exit
+else
+    echo ""
+    echo -e "${RED}Invalid, masukkan pilihan yang benar!${NC}"
+    sleep 2
+    continue
+fi
--- menu/podmanager.sh
+++ menu/podmanager.sh
@@ -16,13 +16,14 @@ while true; do
     echo "script-version   : ${script_version}"
     echo "podman-version   : $(podman --version | awk '{print $3}')"
     echo "==============="
-    echo "1. Create container"
-    echo "2. List all existing container"
-    echo "3. Delete container"
-    echo "4. Pull images"
-    echo "5. List all existing podman images"
-    echo "6. Delete podman images"
-    echo "7. Exit"
+    echo "1. Enter container"
+    echo "2. Create container"
+    echo "3. List all existing container"
+    echo "4. Delete container"
+    echo "5. Pull images"
+    echo "6. List all existing podman images"
+    echo "7. Delete podman images"
+    echo "8. Exit"
     echo "==============="
     echo ""
 
@@ -31,13 +32,14 @@ while true; do
 
     # Sortir input
     case $pilihan in
-    1) clear && podmanager-create-container ; break ;;
-    2) clear && podmanager-list-container ; break ;;
-    3) clear && podmanager-del-container ; break ;;
-    4) clear && podmanager-pull-images ; break ;;
-    5) clear && podmanager-list-images ; break ;;
-    6) clear && podmanager-del-images ; break ;;
-    7) # exit
+    1) clear && podmanager-enter-container ; break ;;
+    2) clear && podmanager-create-container ; break ;;
+    3) clear && podmanager-list-container ; break ;;
+    4) clear && podmanager-del-container ; break ;;
+    5) clear && podmanager-pull-images ; break ;;
+    6) clear && podmanager-list-images ; break ;;
+    7) clear && podmanager-del-images ; break ;;
+    8) exit 0
     *) clear && echo "Silakan masukkan pilihan yang benar!" && sleep 3 && clear ;;
     esac # Menutup case
 
